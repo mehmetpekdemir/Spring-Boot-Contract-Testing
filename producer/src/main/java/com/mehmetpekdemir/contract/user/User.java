0e7f18c157b98c696da844d0057cbfda93ee9249
Mehmet <mehmetpekdemir06@hotmail.com>
2020-12-30 23:00:44 +0300

    Spring Boot Contract Testing.

---
+++ producer/src/main/java/com/mehmetpekdemir/contract/user/User.java
@@ -0,0 +1,23 @@
+package com.mehmetpekdemir.contract.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+@Data
+@Builder
+class User {
+
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String title;
+
+}
